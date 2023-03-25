40d51465549ee31617ddfb4cbdb9c3f0093dc225
Stanciu Angela <125858763+Angela-Stanciu@users.noreply.github.com>
2023-03-25 17:15:22 +0200

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,23 @@
+/*EXEMPLU DE CREARE TABELA , INSERARE VALOARE SI AFISAREA VALORILOR*/
+
+CREATE DATABASE carteDB;
+use carteDB;
+
+CREATE TABLE Student (
+    StudentID INT,
+    Nume VARCHAR(255),
+    Prenume VARCHAR(255),
+    Adresa VARCHAR(255),
+    Oras VARCHAR(255),
+	Telefon INTEGER,
+	Anul_Nasterii DATE
+);
+
+INSERT INTO Student
+VALUES (1, 'Stanciu', 'Angela', 'A.Simu', 'Bucuresti', 0766767889, '01/04/1994');
+
+SELECT * FROM Student;
+
+/*SE VOR STERGE TOATE VALORILE DIN TABELA, DAR NU SI TABELA, EA RAMANAND GOALA*/
+
+TRUNCATE TABLE Student;
